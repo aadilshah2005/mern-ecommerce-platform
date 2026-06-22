e33898d8184eb670d22ee43e1572c2701d38af22
Aadil shah <asadilshah919@gmail.com>
2026-06-22 08:39:03 +0530

    Databse connetion

--- server/src/app.js
+++ server/src/app.js
@@ -1,6 +1,7 @@
 import dotenv from 'dotenv'
 dotenv.config();
 import express from "express";
+import connectDB from './config/db.js';
 
 const PORT = process.env.PORT || 4000;
 
@@ -10,6 +11,17 @@ app.get("/", (req, res) => {
     res.send("Hello Adil shah");
 });
 
-app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`);
-});
+
+const startServer = async () => {
+    try {
+        await connectDB();
+
+        app.listen(PORT, () => {
+            console.log(`Server is running on http://localhost:${PORT}`);
+        });
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+startServer();
